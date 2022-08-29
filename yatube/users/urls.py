02013579aca3aed6aa43d4f27ec9372ca9a5a49c
Андрей Виноградов <vinand96@gmail.com>
2022-08-29 20:13:23 +0500

    project

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -19,32 +19,38 @@ urlpatterns = [
     ),
     path(
         'password_change/',
-        auth.PasswordChangeView.as_view(template_name='users/password_change_form.html'),
+        auth.PasswordChangeView.as_view(
+            template_name='users/password_change_form.html'),
         name='password_change'
     ),
     path(
         'password_change/done/',
-        auth.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
+        auth.PasswordChangeDoneView.as_view(
+            template_name='users/password_change_done.html'),
         name='password_change_done'
     ),
     path(
         'password_reset/',
-        auth.PasswordResetView.as_view(template_name='users/password_reset_form.html'),
+        auth.PasswordResetView.as_view(
+            template_name='users/password_reset_form.html'),
         name='password_reset_form'
     ),
     path(
         'password_reset/done/',
-        auth.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+        auth.PasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html'),
         name='password_reset_done'
     ),
     path(
         'reset/<uidb64>/<token>/',
-        auth.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+        auth.PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html'),
         name='password_reset_confirm'
     ),
     path(
         'reset/done/',
-        auth.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+        auth.PasswordResetCompleteView.as_view(
+            template_name='users/password_reset_complete.html'),
         name='password_reset_complete'
     ),
 ]
